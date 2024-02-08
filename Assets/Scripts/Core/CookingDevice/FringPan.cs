f8dc6f821027f9ebd9076e1c7441f1f8f4993e91
NTB072\danvu <danvuk11@gmail.com>
2024-02-07 22:23:45 -0500

    Add hit logic

--- Assets/Scripts/Core/CookingDevice/FringPan.cs
+++ Assets/Scripts/Core/CookingDevice/FringPan.cs
@@ -6,18 +6,25 @@ namespace Core.CookingDevice
 {
     public class FringPan : MonoBehaviour, IFoodContainer, IStoveCooker
     {
-        private HashSet<FoodObject> _content;
+        [SerializeField]
+        private Transform _foodRoot;
+
+        private HashSet<FoodObject> _content = new();
 
         public void AddFood(FoodObject food)
         {
             bool result = _content.Add(food);
             Debug.Assert(result);
+
+            //food.transform.SetParent(_foodRoot);
         }
 
         public void RemoveFood(FoodObject food)
         {
             bool result = _content.Remove(food);
             Debug.Assert(result);
+
+            //food.transform.SetParent(_foodRoot);
         }
 
         public void StartCooking()
